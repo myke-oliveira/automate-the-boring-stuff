04e04261076ef8c169e701c304f5b506c7fb8739
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2019-09-11 13:38:10 -0300

    Cambridge dictionary works with copy and paste

--- cambridge_dict.py
+++ cambridge_dict.py
@@ -1,8 +1,12 @@
-import webbrowser
+#! /home/myke/anaconda3/bin/python
+
+import webbrowser, pyperclip
 from sys import argv
 
-words = argv.copy()
-words.remove('cambridge_dict.py')
+if len(argv) > 1:
+	words = argv[1:]
+else:
+	words = [pyperclip.paste()]
 
 for word in words:
 	webbrowser.open(f'https://dictionary.cambridge.org/us/dictionary/english-portuguese/{word}')
