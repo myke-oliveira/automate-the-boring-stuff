038a0282a749e0890e7d828ca16a9c47182fc7b3
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2019-09-11 19:25:52 -0300

    Vida de Programador download script

---
+++ downloadVidaDeProgramador.py
@@ -0,0 +1,33 @@
+import requests, bs4, sys, os
+
+url = 'https://vidadeprogramador.com.br/'
+
+directoryname = 'vidadeprogramador'
+
+os.makedirs(directoryname, exist_ok=True)
+
+while True:
+	print(f'Page URL: {url}')
+	res = requests.get(url)
+	try:
+		res.raise_for_status()
+	except Exception as e:
+		print(f'An error has occurred: {e}')
+		sys.exit(1)
+
+	soup = bs4.BeautifulSoup(res.text, 'lxml')
+
+	for tag in soup.select('div.tirinha > a'):
+		imageurl = tag.get('href')
+		print(f'Image URL: {imageurl}')
+		imagefilename = os.path.basename(imageurl)
+		resimage = requests.get(imageurl)
+		try:
+			resimage.raise_for_status()
+		except Exception as e:
+			print(f'An error has occurred: {e}')
+		with open(os.path.join(directoryname, imagefilename), 'wb') as imagefile:
+			for chunk in resimage.iter_content(100000):
+				imagefile.write(chunk)
+
+	url = soup.select('.prev > a')[0].get('href')
