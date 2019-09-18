e5de471df91e4bcf405caf14bbbbf7ac1406eb2e
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2019-09-17 23:24:59 -0300

    Add download pexels script~

---
+++ download_pexels.py
@@ -0,0 +1,30 @@
+import requests, bs4, sys, os
+
+term = input('Type term for searching: ')
+directory = os.path.join('pexels', term)
+os.makedirs(directory, exist_ok=True)
+print(f'Directory {directory} created.')
+url = f'https://www.pexels.com/search/{term}/'
+print(f'Acessing {url}...')
+res = requests.get(url)
+try:
+	res.raise_for_status()
+except Exception as e:
+	print(f'An error has occured: {e}')
+	sys.exit(1)
+print('SUCESS')
+
+soup = bs4.BeautifulSoup(res.text, 'lxml')
+print('Starting...')
+imgtags = soup.find_all('img', class_='photo-item__img')
+for imgtag in imgtags:
+ 	imageurl = imgtag['src']
+ 	print(f'Downloading {imageurl}...')
+ 	imageres = requests.get(imageurl)
+ 	filename = os.path.basename(imageurl)
+ 	print(f'Saving {filename}...')
+ 	for chunk in imageres.iter_content(100000):
+ 		with open(os.path.join(directory, filename), 'wb') as imagefile:
+ 			tam = imagefile.write(chunk)
+ 			print(f'{tam} bytes...')
+ 	print('Saved.')
